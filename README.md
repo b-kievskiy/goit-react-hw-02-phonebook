de8ff118a4ff35c9d17705e9fae404ba3cdb6779
Bogdan <mormul96@ukr.net>
2020-08-10 21:56:30 +0300

    finish

---
+++ README.md
@@ -0,0 +1,2 @@
+# goit-react-hw-02-phonebook
+Home work - 02 React, part 2
---
+++ src/components/ContactForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ContactForm = () => (
+  <>
+    <p>Name</p>
+    <input />
+    <p>Number</p>
+    <input />
+    <button type="submit">Add contact</button>
+  </>
+);
+
+export default ContactForm;
